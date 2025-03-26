a04b42fdcfbc9150009f32441c6ad9c1516ef539
paulMuema <pkmuema87@gmail.com>
2025-03-26 18:13:45 +0300

    Update landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -7,20 +7,18 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex min-h-screen flex-col bg-gray-100">
-        {/* Navbar displayed on all pages */}
-        <Navbar />
+    <div className="flex min-h-screen flex-col bg-gray-100">
+      {/* Navbar displayed on all pages */}
+      <Navbar />
 
-        {/* Page Routes */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
-    </Router>
+      {/* Page Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
   );
 };
 
